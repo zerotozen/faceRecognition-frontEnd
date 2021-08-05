5be32ea94b1ee4eec9881b3ec0f11eef617b929e
zerozen <zerozen@tutamail.com>
2021-08-05 13:54:36 +0200

    change fetch route of register and signin page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
 
   onSubmitSignIn = () => {
     // fetch("https://facerecognition-webpage.herokuapp.com/register", {
-    fetch("https://recognition-web-api.herokuapp.com//register", {
+    fetch("https://recognition-web-api.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
